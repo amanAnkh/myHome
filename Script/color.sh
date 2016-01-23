aa0c29ade5155f92e5f147d0d6d35e6fd5818567
AmanKH <aman.ankh@gmail.com>
2016-01-23 16:35:35 +0000

    ADD = FOLDER/FILE : Script/, info.sh

---
+++ Script/color.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "\033[31mRouge : \033[0m[31m [41m" && echo -e "\033[32mVert : \033[0m[32m [42m"  && echo -e "\033[33mJaune : \033[0m[33m [43m" && echo -e "\033[34mBleu : \033[0m[34m [44m" && echo -e "\033[35mRose : \033[0m[35m [45m" && echo -e "\033[36mCyan : \033[0m[36m [46m" && echo -e "\033[37mGris : \033[0m[37m [47m"
+
